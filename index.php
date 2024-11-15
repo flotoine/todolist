4ba5af5ab7870dac2a55998577c19163b964330a
flotoine <florian.antoine.m@gmail.com>
2024-11-15 17:55:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400..700&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
+    <title>To-Do List</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <h2>You are not signed in</h2>
+        </nav>
+    </header>
+    <main>
+        <h1>Welcome on your To-Do list</h1>
+        <p>Your data could be lost if you remain logged off</p>
+    </main>
+    <footer>
+        <p>All rights reserved - Florian Antoine</p>
+    </footer>
+</body>
+</html>
+
+
